ed355e413e8bd98d35991fb2ba37e33415e20378
nezkart <>
2022-02-26 20:22:47 +0200

    countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await Axios.get(
+      "https://restcountries.com/v2/all?fields=name,region,area"
+    );
+    const products = data;
+    setProducts(products);
+    console.log(products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
     </div>
   );
+
 }
 
 export default App;
